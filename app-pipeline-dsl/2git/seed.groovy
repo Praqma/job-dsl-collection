b86567d425189743c3c2884fc7126381ab9afd44
Thierry Lacour <thi@praqma.net>
2016-05-13 14:59:57 +0200

    Added docs job to 2git

--- app-pipeline-dsl/2git/seed.groovy
+++ app-pipeline-dsl/2git/seed.groovy
@@ -1,5 +1,3 @@
-import javaposse.jobdsl.dsl.jobs.FreeStyleJob
-
 def project_name = '2git'
 def repo_name = 'Praqma/2git'
 def cred_id = "100247a2-70f4-4a4e-a9f6-266d139da9db"
@@ -37,6 +35,48 @@ def integrationJob = job("$project_name-integrate") {
 
     publishers {
         pretestedIntegration()
+        downstreamParameterized {
+            trigger() {
+                parameters {
+                    gitRevision(true)
+                }
+            }
+        }
+    }
+}
+
+def docsJob = job("$project_name-docs") {
+    defaults(delegate)
+    description('updates gh-pages documentation')
+
+    scm {
+        git {
+            remote {
+                github(repo_name)
+                credentials(cred_id)
+            }
+            branch('master')
+            extensions {
+                cleanBeforeCheckout()
+                pruneBranches()
+            }
+        }
+    }
+
+    steps {
+        shell('sha1=$(git subtree split -q --prefix docs master)\n' +
+                'git checkout -q gh-pages\n' +
+                'git merge --ff-only $sha1')
+    }
+
+    publishers {
+        git {
+            pushOnlyIfSuccess()
+            branch('origin', 'gh-pages')
+            tag('origin', 'docs-for-${GIT_REVISION,length=5}') {
+                create()
+            }
+        }
     }
 }
 
